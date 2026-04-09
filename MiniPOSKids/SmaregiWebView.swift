815ee8d258ba610867540ab4a286bd628dbd5699
sasasan03 <sako.hiro62@gmail.com>
2026-04-09 14:39:49 +0900

    add & delete: WebViewの追加。ContentViewの削除

---
+++ MiniPOSKids/SmaregiWebView.swift
@@ -0,0 +1,18 @@
+//
+//  SmaregiWebView.swift
+//  MiniPOSKids
+//
+//  Created by sako0602 on 2026/04/09.
+//
+
+import SwiftUI
+
+struct SmaregiWebView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SmaregiWebView()
+}
