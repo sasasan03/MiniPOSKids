fda801b7d9193242c2968101bcc6d634fba255e0
sasasan03 <sako.hiro62@gmail.com>
2026-04-10 11:27:06 +0900

    refactore: コードラビットの指摘修正

--- MiniPOSKids/SmaregiWebView.swift
+++ MiniPOSKids/SmaregiWebView.swift
@@ -10,21 +10,25 @@ import WebKit
 
 struct SmaregiWebView: View {
     @State private var webPage = WebPage()
+    @State private var didLoad = false
     private let url = URL(string: "https://developers.smaregi.jp/signup/")!
-
+    
     var body: some View {
         ZStack {
             WebView(webPage)
-                .onAppear {
+                .task {
+                    guard !didLoad else { return }
+                    didLoad = true
                     webPage.load(URLRequest(url: url))
                 }
+            
             if webPage.estimatedProgress < 1 {
-                  ProgressView(value: webPage.estimatedProgress)
-                      .progressViewStyle(CircularProgressViewStyle())
+                ProgressView(value: webPage.estimatedProgress)
+                    .progressViewStyle(CircularProgressViewStyle())
             }
         }
     }
-
+    
 }
 
 #Preview {
