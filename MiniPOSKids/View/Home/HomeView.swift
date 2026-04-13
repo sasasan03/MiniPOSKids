ce2e11e1714be49b4c67ec49bd9e3dc044a5987a
sasasan03 <sako.hiro62@gmail.com>
2026-04-13 10:38:53 +0900

    fix: HomeViewにNavigationStackを使わないよう修正

--- MiniPOSKids/View/Home/HomeView.swift
+++ MiniPOSKids/View/Home/HomeView.swift
@@ -8,32 +8,19 @@
 import SwiftUI
 
 struct HomeView: View {
+    
+    @State private var router = HomeRouter()
+    
     var body: some View {
-        NavigationStack {
-            List {
-                Text("Hello, World!")
+        List {
+            Button("登録店舗一覧") {
+                router.navigationHomeRoutePush(.printProductBarcode)
             }
-            .navigationDestination(for: HomeRoute.self) { route in
-                switch route {
-                case .home:
-                    Text("ホーム画面")
-                case .storeList:
-                    Text("登録店舗一覧")
-                case .printProductBarcode:
-                    Text("商品バーコード")
-                case .selectAvailableBalance:
-                    Text("利用可能残高選択画面")
-                case .showBuyerQRCode:
-                    Text("QRコード決済画面（購入者がわ）")
-                case .cashRegister:
-                    Text("レジ画面")
-                case .scanProductBarcode:
-                    Text("バーコード読み取り画面")
-                case .purchaseSuccess:
-                    Text("購入完了画面")
-                case .purchaseFailure:
-                    Text("購入失敗画面")
-                }
+            Button("利用可能残高選択"){
+                router.navigationHomeRoutePush(.showBuyerQRCode)
+            }
+            Button("レジ画面"){
+                router.navigationHomeRoutePush(.cashRegister)
             }
         }
     }
