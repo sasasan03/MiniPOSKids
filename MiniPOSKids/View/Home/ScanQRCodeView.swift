02679b8f451329ab13af0def7a004f71f92cd582
sasasan03 <sako.hiro62@gmail.com>
2026-04-13 14:22:20 +0900

    add: 不足していた画面の追加。コンポーネントの追加

---
+++ MiniPOSKids/View/Home/ScanQRCodeView.swift
@@ -0,0 +1,18 @@
+//
+//  ScanQRCodeView.swift
+//  MiniPOSKids
+//
+//  Created by sako0602 on 2026/04/13.
+//
+
+import SwiftUI
+
+struct ScanQRCodeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ScanQRCodeView()
+}
