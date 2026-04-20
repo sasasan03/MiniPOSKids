747bca168821ed43d5c3138114d6cc68cc2ef4b2
sasasan03 <sako.hiro62@gmail.com>
2026-04-20 13:07:01 +0900

    chore: add Smaregi contract id config

--- MiniPOSKids/AppConfig.swift
+++ MiniPOSKids/AppConfig.swift
@@ -14,6 +14,16 @@ enum AppConfig {
         }
         return value
     }
+    
+    static var smaregiContractId: String {
+        guard let value = Bundle.main.infoDictionary?["SMAREGI_CONTRACT_ID"] as? String,
+              !value.isEmpty,
+              value != "your_contract_id_here" else {
+            fatalError("SMAREGI_CONTRACT_ID が Info.plist に設定されていません。Secrets.xcconfig を確認してください。")
+        }
+        return value
+    }
+
 
     static let oauthCallbackScheme = "miniposkids"
     static let oauthRedirectURI    = "miniposkids://callback"
