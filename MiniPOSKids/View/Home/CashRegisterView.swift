3bc62344e4517dfbb2cb436588f73c49568c3ee0
sasasan03 <sako.hiro62@gmail.com>
2026-05-01 05:31:19 +0900

    add: レジ画面の詳細を記載

--- MiniPOSKids/View/Home/CashRegisterView.swift
+++ MiniPOSKids/View/Home/CashRegisterView.swift
@@ -15,48 +15,91 @@ struct CashRegisterView: View {
         let id = UUID()
         let name: String
         let price: Int
+        let pieces: Int
     }
     
     @State private var items = [
-        Item(name: "りんご", price: 1000),
-        Item(name: "なし", price: 2200)
+        Item(name: "りんご", price: 100, pieces: 5),
+        Item(name: "なし", price: 220, pieces: 3)
     ]
     
+    
     var body: some View {
-        VStack {
-            List {
-                ForEach(items){ item in
-                    HStack {
-                        Text(item.name)
-                        Text("\(item.price)円")
+        ZStack {
+            VStack {
+                List {
+                    ForEach(items){ item in
+                        receiptRow(itemName: item.name, pieces: item.pieces, price: item.price)
                     }
                 }
-            }
-            Spacer()
-            HStack {
-                HStack {
-                    Text("合計")
-                    Text("2200円")
-                }
+                .listStyle(.grouped)
                 Spacer()
-                Button("商品追加") {
-                    router.navigationHomeRoutePush(.scanProductBarcode)
+                HStack {
+                    let total = items
+                        .map { item in item.price * item.pieces }
+                        .reduce(0, +)
+                    totalText(total)
+                    Spacer()
+                    barcodeButton
+                        .padding(.trailing, 5)
                 }
+                .padding()
+                Button(action: {
+                    router.navigationHomeRoutePush(.scanQRCode)
+                }, label: {
+                    Text("支払いを行う")
+                        .font(.system(size: 25))
+                        .frame(width: 300,height: 50)
+                })
+                .overlay(
+                    RoundedRectangle(cornerRadius: 3)
+                        .stroke(.gray, lineWidth: 1)
+                )
             }
-            .padding()
-            Button(action: {
-                router.navigationHomeRoutePush(.scanQRCode)
-            }, label: {
-                Text("支払いを行う")
-                    .frame(width: 300,height: 50)
-                
-            })
-            .overlay(
-                RoundedRectangle(cornerRadius: 3)
-                    .stroke(.gray, lineWidth: 1)
-            )
         }
     }
+    
+    private func receiptRow(itemName: String ,pieces: Int ,price: Int) -> some View {
+        HStack {
+            Text(itemName)
+                .frame(width: 150, alignment: .leading)
+            Spacer()
+            Text("\(pieces)個 × \(price)円・・\(pieces * price)円")
+        }
+        .frame(maxWidth: .infinity)
+    }
+    
+    private func totalText(_ totalPrice: Int) -> some View {
+        HStack {
+            Text("合計")
+                .font(.system(size: 20, weight: .bold))
+                .padding(.horizontal, 5)
+            Text("\(totalPrice.formatted(.number))円")
+                .font(.system(size: 20, weight: .bold))
+        }
+    }
+    
+    private var barcodeButton: some View {
+        Button {
+            router.navigationHomeRoutePush(.scanProductBarcode)
+        } label: {
+            ZStack{
+                Circle()
+                    .frame(width: 50,height: 50)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 45)
+                            .stroke(Color.blue, lineWidth: 1)
+                    )
+                Image(systemName: "barcode.viewfinder")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 30, height: 30)
+                    .foregroundStyle(Color.white)
+            }
+        }
+
+    }
+    
 }
 
 #Preview {
